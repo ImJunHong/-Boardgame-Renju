d4f4d6f93a0e6955e11a75bc7ec5000332fad5d3
ImJunHong <tensai3355@gmail.com>
2021-01-17 16:49:02 +0900

    Second commit

--- renju.py
+++ renju.py
@@ -5,22 +5,19 @@ from rule import Rule
 background_color = (204, 153,   0)
 black            = (  0,   0,   0)
 white            = (255, 255, 255)
+red              = (255,   0,   0)
 
-screen_size = [500, 500]
+screen_size = 500
 cell_size = 30
 stone_size = 13
 margin = 40
-stones = [[0 for y in range(15)] for x in range(15)] # 빈 칸 0, 흑 1, 백 2
-log = []
-rule = Rule(stones, log)
 
 fps = 60
-done = False
 clock = pg.time.Clock()
 
 def main():
     pg.init()
-    screen = pg.display.set_mode(screen_size)
+    screen = pg.display.set_mode([screen_size, screen_size])
     pg.display.set_caption("Renju")
     game = Game(screen)
 
@@ -29,6 +26,9 @@ class Game(object):
         self.screen = screen
         self.is_gameover = False
         self.font = pg.font.SysFont("새굴림", 14)
+        self.forbiddens = []
+        self.is_end = False
+        self.winner = 0
         self.play_game(screen)
 
     def print_text(self, msg, color, pos):
@@ -41,38 +41,53 @@ class Game(object):
     
     def draw_board(self, screen):
         for i in range(15):
-            pg.draw.aaline(screen, black, [margin, margin+cell_size*i], [500-margin, margin+cell_size*i])
-            pg.draw.aaline(screen, black, [margin+cell_size*i, margin], [margin+cell_size*i, 500-margin])
+            pg.draw.aaline(screen, black, [margin, margin+cell_size*i], [screen_size-margin, margin+cell_size*i])
+            pg.draw.aaline(screen, black, [margin+cell_size*i, margin], [margin+cell_size*i, screen_size-margin])
         
-        pg.draw.circle(screen, black, [130, 130], 3)
-        pg.draw.circle(screen, black, [370, 130], 3)
-        pg.draw.circle(screen, black, [130, 370], 3)
-        pg.draw.circle(screen, black, [370, 370], 3)
-        pg.draw.circle(screen, black, [250, 250], 3)
+        pg.draw.circle(screen, black, [margin+cell_size*4, margin+cell_size*4], 3)
+        pg.draw.circle(screen, black, [margin+cell_size*11, margin+cell_size*4], 3)
+        pg.draw.circle(screen, black, [margin+cell_size*4, margin+cell_size*11], 3)
+        pg.draw.circle(screen, black, [margin+cell_size*11, margin+cell_size*11], 3)
+        pg.draw.circle(screen, black, [margin+cell_size*7, margin+cell_size*7], 3)
         
     def draw_stones(self, screen):
-        for count in range(len(log)):
-            x, y = log[count]
+        for count in range(len(self.log)):
+            x, y = self.log[count]
 
-            if stones[y][x] == 1:
+            if self.stones[y][x] == 1:
                 pg.draw.circle(screen, black, [margin+cell_size*x, margin+cell_size*y], stone_size)
-            elif stones[y][x] == 2:
+            elif self.stones[y][x] == 2:
                 pg.draw.circle(screen, white, [margin+cell_size*x, margin+cell_size*y], stone_size)
 
             self.print_text(str(count+1), count%2, (margin+cell_size*x, margin+cell_size*y))
+
+    def draw_forbiddens(self, screen):
+        if len(self.log) % 2 == 0:
+            for x, y in self.forbiddens:
+                half_stone = stone_size//2
+                pg.draw.aaline(screen, red, [margin+cell_size*x-half_stone, margin+cell_size*y-half_stone], [margin+cell_size*x+half_stone, margin+cell_size*y+half_stone])
+                pg.draw.aaline(screen, red, [margin+cell_size*x+half_stone, margin+cell_size*y-half_stone], [margin+cell_size*x-half_stone, margin+cell_size*y+half_stone])
 
     def click(self, screen, pos):
         x, y = pos
         board_x = (x-margin+cell_size//2)//30
         board_y = (y-margin+cell_size//2)//30
-        if board_x >= 0 and board_y >=0 and stones[board_y][board_x] == 0:
-            stones[board_y][board_x] = len(log)%2+1
-            log.append((board_x, board_y))
+        color = len(self.log)%2+1
+        if 0 <= board_x < 15 and 0 <= board_y < 15 and self.stones[board_y][board_x] == 0:
+            if (color == 1 and (board_x, board_y) not in self.forbiddens) or color == 2:
+                self.stones[board_y][board_x] = color
+                self.log.append((board_x, board_y))
+                if self.rule.is_five(board_x, board_y, color=color):
+                    self.winner = color
+                    self.is_end = True
+                else:
+                    self.forbiddens = self.rule.check_forbiddens()
 
     def undo(self):
-        if log:
-            x, y = log.pop()
-            stones[y][x] = 0
+        if self.log:
+            x, y = self.log.pop()
+            self.stones[y][x] = 0
+            self.forbiddens = self.rule.check_forbiddens()
 
     def check_event(self, screen):
         for event in pg.event.get():
@@ -81,24 +96,39 @@ class Game(object):
                     self.click(screen, event.pos)
                 elif event.button == 3:
                     self.undo()
-                self.check_rule()
             elif event.type == pg.QUIT:
                 self.end_game()
 
-    def check_rule(self):
-        for x in range(15):
-            for y in range(15):
-                for direction in range(4):
-                    pass
-
+    def restart(self, screen):
+        for event in pg.event.get():
+            if event.type == pg.MOUSEBUTTONDOWN:
+                if event.button == 3:
+                    self.is_end = False
+            elif event.type == pg.QUIT:
+                self.end_game()
+                
     def play_game(self, screen):
-        while not done:
-            screen.fill(background_color)
-            self.draw_board(screen)
-            self.check_event(screen)
-            self.draw_stones(screen)
-            pg.display.flip()
-            clock.tick(fps)
+        while True:
+            self.stones = [[0 for y in range(15)] for x in range(15)]
+            self.log = []
+            self.forbiddens = []
+            self.rule = Rule(self.stones, self.log)
+            while not self.is_end:
+                screen.fill(background_color)
+                self.draw_board(screen)
+                self.check_event(screen)
+                self.draw_stones(screen)
+                self.draw_forbiddens(screen)
+                pg.display.flip()
+                clock.tick(fps)
+            while self.is_end:
+                screen.fill(background_color)
+                self.draw_board(screen)
+                self.draw_stones(screen)
+                self.print_text(f"{['흑', '백'][self.winner-1]} 승리! 다시 시작하려면 아무 곳이나 우클릭하세요", black, [screen_size//2, screen_size-margin//2])
+                self.restart(screen)
+                pg.display.flip()
+                clock.tick(fps)
 
         self.end_game()
     
--- rule.py
+++ rule.py
@@ -1,92 +1,138 @@
 empty = 0
 black = 1
+white = 2
 directions = [(0, -1), (1, -1), (1, 0), (1, 1)]
-forbiddens = []
 
 class Rule(object):
     def __init__(self, stones, log):
         self.stones = stones
         self.log = log
 
-    def check_validality(self, x, y):
+    def is_valid(self, x, y):
         if 0 <= x < 15 and 0 <= y < 15:
             return True
         return False
-
+    
     def check_forbiddens(self):
+        forbiddens = []
         for x in range(15):
             for y in range(15):
-                if self.stones[y][x] == empty:
-                    pass
-                    
-    def count_stones(self, x, y, direction, color=black, need_shape=False):
-        count = 0
-        shape = ""
-        xx, yy = directions[direction]
-        for i in range(-2, 3):
-            if self.check_validality(x + xx*i, y + yy*i):
-                if self.stones[y + yy*i][x + xx*i] == color:
-                    count += 1
-                    shape += "O"
-                elif self.stones[y + yy*i][x + xx*i] == empty:
-                    shape += "X"
-                else:
-                    shape += "Z"
-            else:
-                return 0, ""
-        if need_shape:
-            return count, shape
-        return count
+                if self.stones[y][x] == empty and self.is_forbidden(x, y):
+                    forbiddens.append((x, y))
+        return forbiddens
 
-    def is_five(self, x, y, direction, color):
-        count = self.count_stones(x, y, direction, color=color)
-        if count == 5:
+    def is_forbidden(self, x, y):
+        if self.is_five(x, y):
+            return False
+        elif self.is_jangmok(x, y) or self.is_fours(x, y) or self.is_threes(x, y):
             return True
         return False
 
-    def is_open_four(self, x, y, direction):
-        count, shape = self.count_stones(x, y, direction, need_shape=True)
-        xx, yy = directions[direction]
-        if count == 4:
-            if shape == "XOOOO":
-                if self.check_validality(x + 3*xx, y + 3*yy) and self.stones[y + 3*yy][x + 3*xx] == empty:
-                    return True
-            elif shape == "OOOOX":
-                if self.check_validality(x - 3*xx, y - 3*yy) and self.stones[y - 3*yy][x - 3*xx] == empty:
+    def count_stones(self, x, y, direction, color=black):
+        dx, dy = directions[direction]
+        xx, yy = x, y
+        count = 1
+        for i in range(2):
+            x, y = xx, yy
+            x += dx*(-1)**i
+            y += dy*(-1)**i
+            while self.is_valid(x, y) and self.stones[y][x] == color:
+                x += dx*(-1)**i
+                y += dy*(-1)**i
+                count += 1
+        return count
+
+    def find_empty(self, x, y, direction, i):
+        dx, dy = directions[direction]
+        dx *= (-1)**i
+        dy *= (-1)**i
+        while self.is_valid(x, y) and self.stones[y][x] == black:
+            x += dx
+            y += dy
+        if self.is_valid(x, y) and self.stones[y][x] == empty:
+            return x, y
+        return None
+
+    def is_jangmok(self, x, y):
+        for direction in range(4):
+            count = self.count_stones(x, y, direction)
+            if count > 5:
+                return True
+        return False
+
+    def is_five(self, x, y, direction=None, color=black):
+        if direction:
+            if self.count_stones(x, y, direction) == 5:
+                return True
+        else:
+            for direction in range(4):
+                count = self.count_stones(x, y, direction, color)
+                if count == 5:
                     return True
         return False
 
-    def is_open_three(self, x, y, direction):
-        count, shape = self.count_stones(x, y, direction, need_shape=True)
-        xx, yy = directions[direction]
-        if count == 3:
-            result = False
-            if shape in ["XXOOO", "OXOOX"]:
-                self.stones[y - yy][x - xx] = black
-                if self.is_open_four(x, y, direction):
-                    result = True
-                self.stones[y - yy][x - xx] = empty
-            elif shape in ["XOXOO", "OOXOX"]:
+    def is_four(self, x, y, direction):
+        for i in range(2):
+            coord = self.find_empty(x, y, direction, i)
+            if coord:
+                if self.is_five(coord[0], coord[1], direction):
+                    return True
+
+    def check_open_four(self, x, y, direction):
+        if self.is_five(x, y):
+            return False
+        count = 0
+        for i in range(2):
+            coord = self.find_empty(x, y, direction, i)
+            if coord:
+                if self.is_five(coord[0], coord[1], direction):
+                    count += 1
+        if count == 2: # ●○●○●○● → ●○●●●○● 이런 경우처럼 한 번의 착수로 같은 direction에서 2개의 4가 생기는 경우에만 해당됨
+            if self.count_stones(x, y, direction) == 4: # 일반적인 open four
+                count = 1
+        else:
+            count = 0
+        return count
+
+    def check_open_three(self, x, y, direction):
+        xx, yy = x, y
+        for i in range(2):
+            x, y = xx, yy
+            coord = self.find_empty(x, y, direction, i)
+            if coord:
+                x, y = coord
                 self.stones[y][x] = black
-                if self.is_open_four(x, y, direction):
-                    result = True
+                # 돌을 추가하였을 때 open four가 되는가?
+                if self.check_open_four(x, y, direction) == 1:
+                    # 돌을 추가한 자리가 다른 이유에 의해 금수가 되어 돌을 추가할 수 없는 자리가 되는가?
+                    if not self.is_forbidden(x, y):
+                        self.stones[y][x] = empty
+                        return True
                 self.stones[y][x] = empty
-            elif shape in ["OOOXX", "XOOXO"]:
-                self.stones[y + yy][x + xx] = black
-                if self.is_open_four(x, y, direction):
-                    result = True
-                self.stones[y + yy][x + xx] = empty
-            elif shape == "XOOOX":
-                self.stones[y - 2*yy][x - 2*xx] = black
-                if self.is_open_four(x, y, direction):
-                    result = True
-                    self.stones[y - 2*yy][x - 2*xx] = empty
-                else:
-                    self.stones[y - 2*yy][x - 2*xx] = empty
-                    self.stones[y + 2*yy][x + 2*xx] = black
-                    if self.is_open_four(x, y, direction):
-                        result = True
-                    self.stones[y + 2*yy][x + 2*xx] = empty
-            if result:
-                return True
+        return False
+
+    def is_fours(self, x, y):
+        count = 0
+        self.stones[y][x] = black
+        for direction in range(4):
+            # 같은 direction에서 두 개의 4가 생길 때
+            if self.check_open_four(x, y, direction) == 2:
+                count += 2
+            # 해당 direction에서 한 개의 4가 생길 때
+            elif self.is_four(x, y, direction):
+                count += 1
+        self.stones[y][x] = empty
+        if count >= 2:
+            return True
+        return False
+
+    def is_threes(self, x, y):
+        count = 0
+        self.stones[y][x] = black
+        for direction in range(4):
+            if self.check_open_three(x, y, direction):
+                count += 1
+        self.stones[y][x] = empty
+        if count >= 2:
+            return True
         return False
